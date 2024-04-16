7bc65cab342b7644f9a1c27c89fd03e08731f9c0
Chigozie-css <chigoziedivine62@gmail.com>
2024-04-16 11:22:35 -0700

    Deleting object

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
         elif 'updated_at' in kwargs:
             kwargs['updated_at'] = str(kwargs['updated_at'])
 
-        self.id = str(uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
